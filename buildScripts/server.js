0af48e214665ccbb014feabf5afb8a1f567ac8ef
Aries McRae <aries@ariesmcrae.com>
2017-02-03 12:49:49 +1100

    Convert node server.js to ES2015

--- buildScripts/server.js
+++ buildScripts/server.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
 
-var port = 3000;
-var app = express();
+const port = 3000;
+const app = express();
 
 app.get('/', function(request, response) {
     response.sendFile(path.join(__dirname, '../src/index.html'));

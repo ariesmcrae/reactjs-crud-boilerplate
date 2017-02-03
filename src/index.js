c3a932592b8e310715e556d4bc2e4d88ecbe91a5
Aries McRae <aries@ariesmcrae.com>
2017-02-03 21:37:33 +1100

    Add css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+import './index.css';
 import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
-
+debugger;
 console.log(`I would pay ${courseValue} for this awesome course!`);

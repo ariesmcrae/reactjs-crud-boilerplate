4dba28bc269d83c28346d0b2af6289a8eee09a91
Aries McRae <aries@ariesmcrae.com>
2017-02-05 14:29:37 +1100

    Add index.html test

--- src/index.test.js
+++ src/index.test.js
@@ -8,6 +8,15 @@ describe('Test index.js', () => {
     });
 });
 
-describe(
-    
-);
+
+describe('index.html', () => {
+    it('should say hello', (done) => {
+        const index = fs.readFileSync('./src/index.html', 'utf-8');
+        jsdom.env(index, function(err, window) {
+            const h1 = window.document.getElementsByTagName('h1')[0];
+            expect(h1.innerHTML).to.equal('hello');
+            done();
+            window.close();
+        });
+    });
+});

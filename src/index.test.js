43444acf1234050a54f2571d1dcb1e8e18e5481a
Aries McRae <aries@ariesmcrae.com>
2017-02-05 14:21:13 +1100

    Add first unit test

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,13 @@
 import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
 
 describe('Test index.js', () => {
-    it('', () => {
+    it('should pass', () => {
         expect(true).to.equal(true);
     });
 });
+
+describe(
+    
+);

d6226c39e90f4b2c2c8cc629c14931c952c697c4
Aries McRae <aries@ariesmcrae.com>
2017-02-06 14:35:06 +1100

    Add ability to switch between mock api and prod api

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,13 @@
 export default function getBaseUrl() {
-    const isInDevelopment = window.location.hostname === 'localhost';
-    return isInDevelopment ? 'http://localhost:3001' : '';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001' : '';
+}
+
+function getQueryStringParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }

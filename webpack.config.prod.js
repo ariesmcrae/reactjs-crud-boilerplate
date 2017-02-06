74652ba522bf3784e6edcdc24c91c073826f4f1c
Aries McRae <aries@ariesmcrae.com>
2017-02-06 21:25:15 +1100

    Add Cache busting

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
 
 export default {
     debug: true,
@@ -19,10 +20,13 @@ export default {
     output: {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/',
-        filename: '[name].js'
+        filename: '[name].[chunkhash].js'
     },
 
     plugins: [
+        // Hash the files using MD5 so that their names change when the content changes.
+        new WebpackMd5Hash(),
+
         //Use CommonsChunkPlugin to create a separate bundle of vendor libraries so that they're cached separately.
         new webpack.optimize.CommonsChunkPlugin({name: 'vendor'}), 
 

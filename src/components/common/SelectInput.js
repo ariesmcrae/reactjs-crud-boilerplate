465cb3813050a2452040cadf24fd20b0757e4a3c
Aries McRae <aries@ariesmcrae.com>
2017-02-21 13:28:13 +1100

    Add reuseable compnent SelectInput.js

---
+++ src/components/common/SelectInput.js
@@ -0,0 +1,37 @@
+import React, {PropTypes} from 'react';
+
+export default function SelectInput({name, label, value, onChange, defaultOption, error, options}) {
+    return(
+        <div className="form-group">
+            <div htmlForm={name}>{label}</div>
+            <div className="field">
+                <select
+                    name={name}
+                    value={value}
+                    className="form-control"
+                    onChange={onChange}
+                >
+                    <option>{defaultOption}</option>
+                    {
+                        options.map(option => {
+                            return <option key={option.value} value={option.value}>{option.text}</option>;
+                        })
+                    }
+                </select>
+                {error && <div className="alert alert-danger">{error}</div>}
+            </div>
+        </div>
+    );
+}
+
+
+
+SelectInput.propTypes = {
+    name: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    defaultOption: PropTypes.string,
+    error: PropTypes.string,
+    options: PropTypes.arrayOf(PropTypes.object)
+};

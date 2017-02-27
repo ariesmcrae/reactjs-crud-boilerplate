8fc9545bcdd521f5a3e3a063cc44c610413fc615
Aries McRae <aries@ariesmcrae.com>
2017-02-27 16:18:38 +1100

    Fix indentation of header.

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,11 +4,11 @@ import { Link, IndexLink } from 'react-router';
 export default function Header() {
     return (
         <nav>
-        <IndexLink to="/" activeClassName="active">Home</IndexLink>
-        {" | "}
-        <Link to="/courses" activeClassName="active">Courses</Link>        
-        {" | "}
-        <Link to="/about" activeClassName="active">About</Link>
+            <IndexLink to="/" activeClassName="active">Home</IndexLink>
+            {" | "}
+            <Link to="/courses" activeClassName="active">Courses</Link>        
+            {" | "}
+            <Link to="/about" activeClassName="active">About</Link>
         </nav>
     );
 }

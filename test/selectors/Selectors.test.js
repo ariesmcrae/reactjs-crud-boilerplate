e59f079965396c5c38cf8c42a861c2faf58bb928
Aries McRae <aries@ariesmcrae.com>
2017-03-02 17:34:38 +1100

    Fix up selector test that wasn't being reported

--- test/selectors/Selectors.test.js
+++ test/selectors/Selectors.test.js
@@ -2,19 +2,20 @@ import {expect} from 'chai';
 import {authorsFormattedForDropdown} from '../../src/selectors/selectors';
 
 describe('Selectors.test.js', () => {
-
     describe('authorsFormattedForDropdown', () => {
-        const authors = [
-            {id: 'cory-house', firstName: 'Cory', lastName: 'House'},
-            {id: 'scott-allen',firstName: 'Scott',lastName: 'Allen'}
-        ];
+        it('should return author data formatted for use in a dropdown', () => {
+            const authors = [
+                {id: 'cory-house', firstName: 'Cory', lastName: 'House'},
+                {id: 'scott-allen',firstName: 'Scott',lastName: 'Allen'}
+            ];
 
-      const expected = [
-        {value: 'cory-house', text: 'Cory House'},
-        {value: 'scott-allen', text: 'Scott Allen'}
-      ];
+            const expected = [
+                {value: 'cory-house', text: 'Cory House'},
+                {value: 'scott-allen', text: 'Scott Allen'}
+            ];
 
-      expect(authorsFormattedForDropdown(authors)).to.eql(expected);
+            expect(authorsFormattedForDropdown(authors)).to.eql(expected);            
+        });
     });
 
 });
